d41c1c2d2c64fdacecb6a369580c1d0e8a9d8b75
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-07-20 22:15:42 +0200

    :sparkles: add meme and random commands

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const TelegramBot = require('node-telegram-bot-api');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const request = require("request");
+const fs = require('fs');
 require('dotenv').config();
 
 const port = 3000;
 const url = 'https://api.telegram.org/bot';
+const donald_trump_api = 'https://api.tronalddump.io';
 const TOKEN = process.env.API_KEY;
 
 // import constants
@@ -34,3 +38,68 @@ bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, greetings);
 });
 
+bot.onText(/\/search/, (msg) => {
+  bot.sendSticker(msg.chat.id, 'CAADAgAD8QYAAnlc4gkl_dcUtPY2AgI');
+  bot.sendMessage(msg.chat.id, 'Amazing. Send me the word you want me to search for');
+
+  bot.on('message', (req) => {
+    axios.get(donald_trump_api + '/search/quote', {
+      params: {
+        query: req.text
+      }
+    })
+    .then((response) => {
+      //bot.sendMessage(msg.chat.id, `${response.data.count}`);
+    })
+    .catch((error) => {
+      bot.sendMessage(msg.chat.id, 'Something went wrong. Try later');
+      console.log('/search', error);
+    });
+  });
+});
+
+bot.onText(/\/random/, (msg) => {
+  axios.get(donald_trump_api + '/random/quote')
+  .then((response) => {
+    bot.sendSticker(msg.chat.id, 'CAADAgAD7QYAAnlc4gnK88QdYpKR7AI');
+    bot.sendMessage(msg.chat.id, `${response.data.value}`);
+  })
+  .catch((error) => {
+    bot.sendMessage(msg.chat.id, 'Something went wrong. Try later');
+    console.log('/random', error);
+  });
+});
+
+bot.onText(/\/meme/, (msg) => {
+  // axios.get(donald_trump_api + '/random/meme')
+  // .then((response) => {
+  //   bot.sendSticker(msg.chat.id, 'CAADAgAD7AYAAnlc4gmyzyRQT6BJSwI');
+  //   bot.sendPhoto(msg.chat.id, response.data.toString());
+  // })
+  // .catch((error) => {
+  //   bot.sendMessage(msg.chat.id, 'Something went wrong. Try later');
+  //   console.log('/meme ', error);
+  // });
+  // const filename = "tmp_pic/"+microtime.now()+".png"; 
+  // const test_link = 'https://api.tronalddump.io/random/meme';
+  // // bot.sendPhoto(msg.chat.id, `https://api.tronalddump.io/random/meme`);
+  // request(test_link).pipe(fs.createWriteStream(filename).on('close', function() {
+  //   // done download file
+  //   bot.sendPhoto(msg.chat.id, filename).then(function(){
+  //   // photo sent, deleted temp file
+  //     fs.unlinkSync(filename);
+  //   });
+  // }));
+
+  axios.get(donald_trump_api + '/random/meme', {
+    responseType: 'stream'
+  })
+  .then((response) => {
+    response.data.pipe(fs.createWriteStream('image.jpg'));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+  bot.sendPhoto(msg.chat.id, 'image.jpg');
+});
